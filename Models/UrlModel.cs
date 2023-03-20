240f750cecc519356b0b988285f3fc0d05b6ccde
James Joyce <james.joyce@shipadmin.com>
2023-03-20 13:52:43 +0800

    Add project files.

---
+++ Models/UrlModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Url_Shortener.Models
+{
+    public class UrlModel
+    {
+        public int Id { get; set; }
+        public string OriginalUrl { get; set; }
+        public string ShortenedUrl { get; set; }
+
+    }
+
+
+}
